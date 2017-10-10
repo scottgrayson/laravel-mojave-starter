8aff148c3f40f008657d3d19f318319f3aa60ccf
Scott Grayson <dscottgrayson@gmail.com>
2017-10-10 14:10:44 -0400

    change names in config to betty

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>
-      {{ config('app.name', 'Mojave') }} - @yield('title')
+      {{ config('app.name', "Miss Betty's Day Camp") }} - @yield('title')
     </title>
 
     <meta name="description" content="@yield('description')">
