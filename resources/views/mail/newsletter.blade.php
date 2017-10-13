17e320702cea2dc72729caf887b1e341986d7982
Scott Grayson <dscottgrayson@gmail.com>
2017-10-13 14:38:41 -0400

    newsletter template

---
+++ resources/views/mail/newsletter.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+
+  {!! $newsletter->body !!}
+
+  <p style="font-size:0.65rem; text-align:center;">
+    <a href="{{ route("newsletter.delete", $data['subscriberId']) }}">
+      Unsubscribe
+    </a>
+  </p>
+
+  <img src="{{ $data['links']['tiny'] }}"/>
+
+@endcomponent
