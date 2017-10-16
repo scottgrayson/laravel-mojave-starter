5877959604539e2d79a7929c471a15d7c94878b7
jacob zlogar <jacob.zlogar@gmail.com>
2017-10-16 12:48:53 -0400

    product crud

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Requests;
 
-class PageRequest extends FormRequest
+class ProductRequest extends FormRequest
 {
     public function rules()
     {
         return [
-            'name' => 'required',
-            'title' => 'required',
-            'uri' => 'required',
-            'published' => 'boolean',
-            'meta_title' => 'nullable',
-            'meta_description' => 'nullable',
-            'meta_tags' => 'nullable',
-            'content' => 'nullable',
+            'name' => 'string|required',
+            'price' => 'integer|required',
+            'desc' => 'string|required',
         ];
     }
 }
