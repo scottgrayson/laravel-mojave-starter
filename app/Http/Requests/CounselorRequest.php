3b5041be06fa30b9892df542a472f694661a199f
jacob zlogar <jacob.zlogar@gmail.com>
2017-10-19 11:47:38 -0400

    phpcs

--- app/Http/Requests/CounselorRequest.php
+++ app/Http/Requests/CounselorRequest.php
@@ -4,14 +4,16 @@ namespace App\Http\Requests;
 
 class CounselorRequest extends FormRequest
 {
-    public function adminCreateRules() {
+    public function adminCreateRules()
+    {
         return [
             'user_id' => 'required|numeric',
             'tent_id' => 'required|numeric',
         ];
     }
 
-    public function adminEditRules() {
+    public function adminEditRules()
+    {
         return [
             'user_id' => 'required|numeric',
             'tent_id' => 'required|numeric',
