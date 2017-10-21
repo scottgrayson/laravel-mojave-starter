e07347086cb62394a7bc77ea421d5a5ba9c7a405
Scott Grayson <dscottgrayson@gmail.com>
2017-10-21 10:01:38 -0400

    get the camper status

--- app/Camper.php
+++ app/Camper.php
@@ -13,4 +13,24 @@ class Camper extends Model
     {
         return $this->belongsTo(\App\User::class);
     }
+
+    public function reservations()
+    {
+        return $this->hasMany(\App\Reservation::class);
+    }
+
+    public function getStatusAttribute()
+    {
+        $rules = (new \App\Http\Requests\CamperRequest)->editRules();
+        $requiredFields = $rules;
+        $requiredValues = array_intersect_key($this->toArray(), $requiredFields);
+
+        if (in_array(null, $requiredValues)) {
+            return 'Registration Incomplete';
+        } elseif ($this->reservations->isEmpty()) {
+            return 'No Reservations';
+        } else {
+            return 'Camp Dates Reserved';
+        }
+    }
 }
