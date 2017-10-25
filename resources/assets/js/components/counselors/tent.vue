73439e31e4aa2494e4735246e01b24a0b7baeb79
jacob zlogar <jacob.zlogar@gmail.com>
2017-10-25 13:29:12 -0400

    wip

--- resources/assets/js/components/counselors/tent.vue
+++ resources/assets/js/components/counselors/tent.vue
@@ -4,7 +4,8 @@
     <div class="card-body">
       <div class="d-flex flex-row justify-content-between align-items-center mb-3">
         <a class="btn btn-outline-secondary"
-          :class="[campStart === selectedWeek ? 'disabled' : '']">
+          @click="fetchPrevious"
+          :class="[campStart === weeks[selectedWeek][0].date ? 'disabled' : '']">
           Previous
         </a>
         <div class="dropdown">
@@ -13,12 +14,14 @@
           </button>
           <div class="dropdown-menu">
             <a class="dropdown-item" v-for="(value, key) in weeks"
-              @click="selectedWeek = value[0].date">
-              Week: {{value[0].date}}</a>
+              @click="selectedWeek = key">
+              {{weeks[key][0].date}}
+            </a>
           </div>
         </div>
         <a class="btn btn-outline-secondary"
-          :class="[campEnd === selectedWeek ? 'disabled' : '']">
+          @click="fetchNext"
+          :class="[campEnd === weeks[selectedWeek][0].date ? 'disabled' : '']">
           Next
         </a>
       </div>
@@ -46,15 +49,23 @@
         campers: {},
         campStart: '',
         campEnd: '',
-        selectedWeek: ''
+        selectedWeek: 0
       }
     },
     computed: {
       weekOf () {
-        return 'Week Of: '+moment(this.selectedWeek).format('MMMM D, YYYY')
+        return 'Week Of: '+moment(this.weeks[this.selectedWeek][0].date).format('MMMM D, YYYY')
       }
     },
     methods: {
+      fetchNext () {
+        this.selectedWeek += 1
+        this.getReservations(this.selectedWeek)
+      },
+      fetchPrevious () {
+        this.selectedWeek -= 1
+        this.getReservations(this.selectedWeek)
+      },
       getReservations (week) {
         axios.get('/api/reservations/week/'+week)
           .then((response) => {
@@ -70,7 +81,6 @@
       axios.get('/api/camp-dates/')
         .then((response) => {
           this.weeks = response.data.weeks
-          this.selectedWeek = response.data.weeks[0][0].date
           this.campStart = response.data.camp_start.date
           this.campEnd = response.data.camp_end.date
         })
