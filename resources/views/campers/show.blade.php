4d2800214f855a50ce0d85eb1bc208a3c994fd8a
jacob zlogar <jacob.zlogar@gmail.com>
2017-10-31 15:21:25 -0400

    basic show page for now, can add details as we need

--- resources/views/campers/show.blade.php
+++ resources/views/campers/show.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-  <div>
-    {{ $camper->name }}
+  <div class="card">
+    <div class="card-header">
+      <p class="lead text-center">
+        {{ $camper->name }}
+      </p>
+    </div>
+    <div class="card-content">
+      @if ($camper->allergies !== null)
+        <p class="text-center mt-1">Allergies</p>
+        <ul>
+          <li>{{$camper->allergies}}</li>
+        </ul>
+      @endif
+    </div>
   </div>
 @endsection
