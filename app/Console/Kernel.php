b7bba85215f491fb9105b4b9095a2f6166ae12f2
jacob zlogar <jacob.zlogar@gmail.com>
2017-11-01 14:46:55 -0400

    scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\CounselorReminder;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->job(new CounselorReminder)->sundays()->at('12:00');
     }
 
     /**
