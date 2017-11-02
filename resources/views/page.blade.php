5d3943dea403b3081d691543006bc10656974d3b
Scott Grayson <dscottgrayson@gmail.com>
2017-11-01 20:41:20 -0400

    default styles to make existing content look better

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,16 +5,24 @@
 @endsection
 
 @section('content')
-  {!! $page->html !!}
+  <div class="page-content">
+    {!! $page->html !!}
+  </div>
 @endsection
 
 
 @section('style')
   @parent
   <style>
-    img {
-      display: block;
+    .page-content {
+      font-size: 1.125rem;
       margin: 0 auto;
+      padding-bottom: 2rem;
+      max-width: 800px;
+    }
+    .page-content img {
+      display: block;
+      margin: 1rem auto;
       max-height: 50vh;
       max-width: 100%;
     }
