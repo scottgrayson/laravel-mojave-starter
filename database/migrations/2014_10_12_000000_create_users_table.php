652519589f8e94119ba0f878ea3cecf7ac8bec0e
Scott Grayson <dscottgrayson@gmail.com>
2017-11-11 12:00:51 -0500

    remove unneeded user card columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,11 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->boolean('notify_email')->default(true);
-
             $table->string('braintree_customer')->nullable()->index();
-            $table->string('card_brand')->nullable();
-            $table->string('card_last_four')->nullable();
-
             $table->string('password');
             $table->boolean('invite_pending')
                 ->default(false);
