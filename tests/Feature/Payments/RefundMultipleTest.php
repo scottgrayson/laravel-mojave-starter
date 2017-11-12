b7ea2ffe3ae6717e2c40d6a6730a16df581e4751
Scott Grayson <dscottgrayson@gmail.com>
2017-11-12 12:03:01 -0500

    wip refund work party attendees

--- tests/Feature/Payments/RefundMultipleTest.php
+++ tests/Feature/Payments/RefundMultipleTest.php
@@ -8,6 +8,7 @@ use App\Camp;
 use App\User;
 use App\Payment;
 use Braintree_Transaction;
+use Braintree\Test\Transaction;
 
 class RefundMultipleTest extends TestCase
 {
@@ -17,37 +18,56 @@ class RefundMultipleTest extends TestCase
 
     public function testRefunding()
     {
-        $users = factory(User::class, 3)->create();
         $camp = factory(Camp::class)->create();
 
-        foreach ($users as $user) {
-            $result = Braintree_Transaction::sale([
-                'amount' => '10.00',
-                'paymentMethodNonce' => 'fake-valid-nonce',
-                'options' => [
-                    'submitForSettlement' => true,
-                ],
-            ]);
+        $usersWithRefund = factory(User::class, 1)->create()
+            ->each(function ($user) use ($camp) {
+                $this->createPayment($user, $camp, true);
+            })
+            ->pluck('email');
 
-            $payment = factory(Payment::class)->create([
-                'user_id' => $user->id,
-                'camp_id' => $camp->id,
-                'transaction' => $result->transaction->id,
-                'type' => 'registration_fee',
-                'amount' => '10.00',
-            ]);
-        }
+        $usersWithPayment = factory(User::class, 2)->create()
+            ->each(function ($user) use ($camp) {
+                $this->createPayment($user, $camp, false);
+            })
+            ->pluck('email');
 
-        $this->be($user);
+        $usersWithoutPayment = factory(User::class, 3)->create()->pluck('email');
+
+        $emails = User::pluck('email')->push('junk@email.com')->implode(',');
 
         $r = $this->json('POST', route('admin.payments.refund-multiple'), [ 'emails' => $emails ]);
 
         //$this->feedback($r);
         $r->assertStatus(200);
 
-        $r->assertJsonFragment(['already_refunded' => []]);
-        $r->assertJsonFragment(['refunded' => []]);
-        $r->assertJsonFragment(['email_not_found' => []]);
-        $r->assertJsonFragment(['payment_not_found' => []]);
+        $r->assertJsonFragment(['already_refunded' => $usersWithRefund]);
+        $r->assertJsonFragment(['refunded' => $usersWithPayment]);
+        $r->assertJsonFragment(['email_not_found' => ['junk@email']]);
+        $r->assertJsonFragment(['payment_not_found' => $usersWithoutPayment]);
+    }
+
+    protected function createPayment($user, $camp, $refunded = false)
+    {
+        $result = Braintree_Transaction::sale([
+            'amount' => '10.00',
+            'paymentMethodNonce' => 'fake-valid-nonce',
+            'options' => [
+                'submitForSettlement' => true,
+            ],
+        ]);
+
+        Transaction::settle($result->transaction->id);
+        if ($refunded) {
+            Braintree_Transaction::refund($result->transaction->id);
+        }
+
+        $payment = factory(Payment::class)->create([
+            'user_id' => $user->id,
+            'camp_id' => $camp->id,
+            'transaction' => $result->transaction->id,
+            'type' => 'registration_fee',
+            'amount' => '10.00',
+        ]);
     }
 }
