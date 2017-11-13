3d2f039da3951e6e4bd88573e63bbcc12521d64e
Scott Grayson <dscottgrayson@gmail.com>
2017-11-12 19:22:35 -0500

    unique NewsletterSubscriberFactory email

--- database/factories/NewsletterSubscriberFactory.php
+++ database/factories/NewsletterSubscriberFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\NewsletterSubscriber::class, function (Faker $faker) {
     return [
-        'email' => $faker->email,
+        'email' => $faker->unique()->email,
     ];
 });
