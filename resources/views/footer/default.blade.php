99a215bebddc7e44057cdd213e30637e40b6ab1e
Scott Grayson <dscottgrayson@gmail.com>
2017-11-15 18:14:22 -0500

    better footer nav

--- resources/views/footer/default.blade.php
+++ resources/views/footer/default.blade.php
@@ -1,18 +1,20 @@
 <footer class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="col d-none d-sm-inline">
+  <div class="container h-100">
+    <div class="row h-100">
+      <div class="col h-100 d-none d-md-flex align-items-center">
         <img height="80px" src="{{ asset('uploads/desert_logo.svg') }}"/>
-        <h3 class="d-inline pl-2">
+        <h3 class="d-inline pl-3">
           {{ config('app.name') }}
         </h3>
       </div>
-      <div class="col text-right">
-        @foreach(\App\MenuItem::childrenOf('footer') as $item)
-          <a href="{{$item->href}}">
-            {{ title_case($item->label) }}
-          </a>
-        @endforeach
+      <div class="col h-100">
+        <nav class="nav h-100 justify-content-end align-items-center">
+          @foreach(\App\MenuItem::childrenOf('footer') as $item)
+            <a class="nav-link" href="{{$item->href}}">
+              {{ title_case($item->label) }}
+            </a>
+          @endforeach
+        </nav>
       </div>
     </div>
   </div>
