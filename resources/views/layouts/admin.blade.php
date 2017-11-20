47e73d15c872fcae91cf40651c122857430924f4
Scott Grayson <dscottgrayson@gmail.com>
2017-11-20 16:35:50 -0500

    fixed sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,20 +2,16 @@
 
 @section('body')
   @include('admin.nav.top')
-  <div class="container-fluid">
-    <div class="row">
-      <div class="p-0 col-lg-2 d-none d-lg-block">
-        @include('admin.nav.sidebar')
-      </div>
-
-      <main class="col-lg-10 p-0" role="main">
-        @include('partials.breadcrumbs')
-
-        <div class="container-fluid">
-          @yield('content')
-        </div>
-
-      </main>
-    </div>
+  <div class="d-none d-lg-block">
+    @include('admin.nav.sidebar')
   </div>
+
+  <main class="admin-content" role="main">
+    @include('partials.breadcrumbs')
+
+    <div class="container-fluid">
+      @yield('content')
+    </div>
+
+  </main>
 @endsection
