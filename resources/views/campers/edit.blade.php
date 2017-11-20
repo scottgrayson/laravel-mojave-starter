a16ec333ddac17023e2eb475cd0616bd6037253d
Scott Grayson <dscottgrayson@gmail.com>
2017-11-20 10:08:27 -0500

    update henna/photo consent wording

--- resources/views/campers/edit.blade.php
+++ resources/views/campers/edit.blade.php
@@ -31,10 +31,12 @@ $steps = [
 $wording = [
   'tent_id' => ['help' => 'Choose the grade your child attended as of January of this year.'],
   'photo_consent' => [
-    'before' => "We take photographs of camp activities, that usually include recognizable images  of our campers, as well as counselors and staff.  We would like to use these photographs for marketing purposes, including, but not limited to, in brochures,  mailings and on our website,  and to include them in CD albums of the Camp experience that may be offered for sale to Camp families at the end of Camp.   By signing below, you agree that Miss Betty’s Day Camp is granted a license to use photographs that include images of your child/children in their activities at Camp for those purposes,  without charge, unless the box below has been checked.  Thank you.",
+    'label' => "Miss Betty's Day Camp may use photos of " . $item->name,
+    'before' => "We take photographs of camp activities, that usually include recognizable images  of our campers, as well as counselors and staff.  We would like to use these photographs for marketing purposes, including, but not limited to, in brochures,  mailings and on our website,  and to include them in CD albums of the Camp experience that may be offered for sale to Camp families at the end of Camp.   By checking the box below, you agree that Miss Betty’s Day Camp is granted a license to use photographs that include images of your child/children in their activities at Camp for those purposes.",
   ],
   'henna_consent' => [
-    'before' => "Henna consent explanation",
+    'label' => $item->name . ' has permission to obtain a henna tattoo',
+    'before' => "On Fridays the Clay Barn at Miss Betty’s Day Camp will offer optional henna tattoo designs, with your permission. We have a variety of child-friendly designs for arms or legs only. The henna used is from www.HENNACARAVAN.com and does not contain any black or colored chemical dyes or additives, PPD paraphnylenediamene, nut products or preservatives. It contains 100% natural henna, sugar, lemon juice and essential oils (cajeput, orange sweet, wild orange or lavender). We will also use sugar, lemon juice/tea tree oil to help seal the design. Anyone with allergy concerns or other concerns should consult a doctor and/or hennacaravan.com for further questions. The design is applied with a paste that dries and can be washed off, leaving behind the brown design stain. Depending on how long the paste is left on (we recommend 6 hours) the design will last 7 to 14 days, sometimes less.",
   ],
   'allergies' => [
     'before' => '<b>Campers with allergies must have a completed paper copy of <a target="_blank" href="/assets/MBDC.medical.pdf">THIS FORM</a> when they attend camp.</b>',
