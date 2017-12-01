980b77376d61aec4c1ec3adb4352268ae4e22407
Scott Grayson <dscottgrayson@gmail.com>
2017-12-01 17:10:13 -0500

    test for tent limit blocking reservations

---
+++ tests/Feature/Payments/CampIsFullTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature\Payments;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Camp;
+use App\Camper;
+use App\Product;
+use App\User;
+use App\Tent;
+use App\Reservation;
+use App\TentLimit;
+use App\Payment;
+use Cart;
+use Carbon\Carbon;
+
+class PayAndReserveTest extends TestCase
+{
+    public function testPayingAndReservingCamperDays()
+    {
+        $product = factory(Product::class)->create(['slug' => 'day']);
+        $registrationFee = factory(Product::class)->create(['slug' => 'registration-fee']);
+        $tent = factory(Tent::class)->create();
+        $user = factory(User::class)->create();
+        $camp = factory(Camp::class)->create();
+        $camper = factory(Camper::class)->create([
+            'tent_id' => $tent->id,
+            'user_id' => $user->id,
+        ]);
+        $otherCamper = factory(Camper::class)->create([
+            'tent_id' => $tent->id,
+            'user_id' => $user->id,
+        ]);
+
+        // make tent limit one for a certain day and try to reserve twice
+        $day = $camp->randomCampDay();
+        $reservation = factory(Reservation::class)->create([
+            'payment_id' => factory(\App\Payment::class)->create([
+            'camp_id' => $camp->id,
+            ])->id,
+            'tent_id' => $tent->id,
+            'user_id' => $user->id,
+            'camper_id' => $otherCamper->id,
+            'date' => $day,
+        ]);
+
+        TentLimit::create([
+            'tent_id' => $tent_id,
+            'date' => $day,
+            'limit' => 1,
+        ]);
+
+        $this->be($user);
+
+        Cart::add($product, 1, [
+            'camper_id' => $c->id,
+            'tent_id' => $tent->id,
+            'product' => $product->slug,
+            'date' => $day,
+        ]);
+
+        $r = $this->post(route('api.payments.store'), [
+            'nonce' => 'fake-valid-nonce',
+        ]);
+
+        $r->assertStatus(200);
+
+        $this->assertEquals($user->reservations->count(), 1);
+    }
+}
