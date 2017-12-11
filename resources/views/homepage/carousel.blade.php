b1885a16ef1eb2249deed903182b8f6c5ff12db2
Scott Grayson <dscottgrayson@gmail.com>
2017-12-11 17:45:52 -0500

    fix carousel

--- resources/views/homepage/carousel.blade.php
+++ resources/views/homepage/carousel.blade.php
@@ -1,16 +1,21 @@
 <div id="carousel" class="homepage-carousel carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
+  </ol>
   <div style="height:100vh" class="carousel-inner">
     <div class="carousel-item active" style="background-image:url('/img/circle.jpg')" alt="First slide">
     </div>
-    <div class="carousel-item active" style="background-image:url('/img/woodshop.jpg')" alt="Second slide">
+    <div class="carousel-item" style="background-image:url('/img/woodshop.jpg')" alt="Second slide">
     </div>
-    <div class="carousel-item active" style="background-image:url('/img/claybarn.jpg')" alt="Third slide">
+    <div class="carousel-item" style="background-image:url('/img/claybarn.jpg')" alt="Third slide">
     </div>
   </div>
   <div class="carousel-caption">
     <h1>Miss Betty's Day Camp</h1>
     <p>Chester Springs, PA</p>
-    <a class="btn btn-primary" href="/campers">Enroll</a>
+    <a class="btn-lg btn btn-primary" href="/campers">Enroll</a>
   </div>
 
 </div>
