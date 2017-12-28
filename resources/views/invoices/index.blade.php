f31671d65b195c650cc3a7a091dd62453197adab
jacob zlogar <jacob.zlogar@gmail.com>
2017-12-28 12:40:43 -0500

    Table

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -4,10 +4,19 @@
   <table class="table table-responsive-md">
     <thead>
       <tr>
-        <th scope="col"></th>
+        <th scope="col">Date</th>
+        <th scope="col">Total</th>
       </tr>
     </thead>
+    <tbody>
     @foreach($invoices as $i)
-
+      <tr>
+        <td scope="row"><a href="/invoices/"+{{$i->id}}>
+            {{\Carbon\Carbon::parse($i)->diffForHumans()}}
+        </a></td>
+        <td scope="row">{{$i->total}}</td>
+      </tr>
     @endforeach
+  </tbody>
+  </table>
 @endsection
