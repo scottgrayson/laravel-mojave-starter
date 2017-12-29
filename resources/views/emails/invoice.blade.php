dd9e19e41a543d729b3f8a7c7fceb4e984113c23
jacob zlogar <jacob.zlogar@gmail.com>
2017-12-29 13:53:22 -0500

    str_plural helper

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -3,7 +3,7 @@
 
   Dear {{$user->name}},  
 
-  Thank you for your reservation!
+  Thank you for your {{str_plural('reservation', $invoice->reservations->count())}}
 
   - You reserved {{$invoice->reservations->first()->camper->first_name}} for: {{$invoice->reservations->count() . ' days'}}
 
