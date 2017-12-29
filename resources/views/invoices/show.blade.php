5d2b0d5aed9e354ed346c59123b76e27497e0781
jacob zlogar <jacob.zlogar@gmail.com>
2017-12-29 13:31:27 -0500

    use the relation

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -4,8 +4,8 @@
   <div>
     <div class="card">
       <h5 class="card-title text-center py-2">
-        {{\App\Camper::find($invoice->reservations->first()->camper_id)->first_name}}
-        {{\App\Camper::find($invoice->reservations->first()->camper_id)->last_name}}
+        {{$invoice->reservations->first()->camper->first_name}}
+        {{$invoice->reservations->first()->camper->last_name}}
       </h5>
       <p class="text-center">
         {{$invoice->reservations->count()}} days reserved
