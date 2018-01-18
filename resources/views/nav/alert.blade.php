61d8be9f685ad888d165169d5e0f25747a9cae39
jacob zlogar <jacob.zlogar@gmail.com>
2018-01-18 15:16:34 -0500

    WIP

---
+++ resources/views/nav/alert.blade.php
@@ -0,0 +1,6 @@
+<div class="container-fluid m-0" style="padding-top:98px; padding-right: 0px; padding-left: 0px;">
+  <div class="alert alert-success mt-0 mb-0 text-center">
+    Registration Closes {{\App\Camp::current()->registration_end->diffForHumans()}}. Click 
+    <a href="/register">Here</a> To Register
+  </div>
+</div>
