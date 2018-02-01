902724195b55b44f357f72c6ea19c603dd2bb95e
jacob zlogar <jacob.zlogar@gmail.com>
2018-02-01 12:48:52 -0500

    wip

---
+++ tests/Feature/Payments/PaymentReminderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Payments;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Carbon\Carbon;
+
+class PaymentReminderTest extends TestCase
+{
+    public function testSendingReminderEmails()
+    {
+
+    }
+}
