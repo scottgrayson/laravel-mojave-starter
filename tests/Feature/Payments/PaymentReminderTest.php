5812620d78cfaeffb6f910c4a09a134d58be7900
jacob zlogar <jacob.zlogar@gmail.com>
2018-02-09 13:15:03 -0500

    payment reminder test

--- tests/Feature/Payments/PaymentReminderTest.php
+++ tests/Feature/Payments/PaymentReminderTest.php
@@ -3,13 +3,35 @@
 namespace Tests\Feature\Payments;
 
 use Tests\TestCase;
+use App\Camper;
+use App\User;
+use App\Tent;
+
+use App\Mail\PaymentReminderMail as Reminder;
+use App\Jobs\PaymentReminder;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Carbon\Carbon;
 
 class PaymentReminderTest extends TestCase
 {
-    public function testSendingReminderEmails()
+    public function testSendingReminder()
     {
+        Mail::fake();
 
+        $user = factory(User::class)->create();
+        $user2 = factory(User::class)->create();
+        $tent = factory(Tent::class)->create();
+        $camper = factory(Camper::class)->create([
+            'tent_id' => $tent->id,
+            'user_id' => $user->id,
+        ]);
+
+        PaymentReminder::dispatch();
+
+        Mail::assertSent(Reminder::class, function($mail) use ($user) {
+            return $mail->user->id === $user->id;
+        });
+
+        Mail::assertSent(Reminder::class, 1);
     }
 }
