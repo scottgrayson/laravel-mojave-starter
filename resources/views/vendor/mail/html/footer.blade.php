ac7c3f4813a8ca668e73dfa5d2da79237df1a835
jacob zlogar <jacob.zlogar@gmail.com>
2018-02-12 12:26:45 -0500

    added icon to the footer

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -3,6 +3,7 @@
         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
             <tr>
                 <td class="content-cell" align="center">
+                  <img height="75px" style="margin: 1rem;" src="{{ asset('img/logo.png') }}"/>
                     {{ Illuminate\Mail\Markdown::parse($slot) }}
                 </td>
             </tr>
