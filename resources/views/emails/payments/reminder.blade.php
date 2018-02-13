2a42253db4320fb841dbc184dd54dc0325b28f01
jacob zlogar <jacob.zlogar@gmail.com>
2018-02-13 11:05:34 -0500

    reservations for campers

--- resources/views/emails/payments/reminder.blade.php
+++ resources/views/emails/payments/reminder.blade.php
@@ -2,7 +2,7 @@
 # Registration
 
 Dear {{$user->name}}, camp registration closes {{$camp->camp_start->diffForHumans()}},
-we noticed you haven't completed payment for your reservations,
+we noticed you haven't completed reservation for your campers,
 follow the link below to finish reservation. 
 
 @component('mail::button', ['url' => $url])
