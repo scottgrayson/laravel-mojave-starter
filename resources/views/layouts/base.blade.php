6a3c15bf498afed9d9083a7282b601c8544c0c49
Steve Williamson <steve@sjdub.com>
2018-05-28 15:36:31 -0400

    Update snippet

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -47,7 +47,8 @@
     </script>
 
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
   </head>
